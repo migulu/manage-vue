939bcedba011c5338128cb444cc8b63f04dd7973
allan <allan@migulu.com>
2021-02-24 10:53:14 +0800

    vuetify 已安裝

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="這是傳進去的 msg "/>
   </div>
 </template>
 
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="account"
+            :counter="10"
+            label="帳號"
+            required
+          ></v-text-field>
+         
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+        <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="密碼"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        show1: false,
+        show2: true,
+        show3: false,
+        show4: false,
+        password: '',
+        account: '',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => ('The email and password you entered don\'t match'),
+        },
+      }
+    },
+  }
+</script>
